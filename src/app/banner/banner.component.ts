9c999b6e7986bf339ea13ed9148a87a3a599711e
Karthik707 <karthikkulal707@gmail.com>
2022-09-24 14:07:21 +0530

    commit 3

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
+  router: any;
 
   constructor() { }
-
+  onClick(){
+    this.router.navigate(['fashiondeals'])
+  }
   ngOnInit(): void {
   }
 
